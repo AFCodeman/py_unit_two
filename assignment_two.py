0f95b66441035479c877b229b504723e94c6b74a
anthonyfagiolo <fagioan@ssfs.org>
2021-09-21 13:45:22 -0400

    finished my unit two project. I like the final product, and am unapologetic about the caps.

--- assignment_two.py
+++ assignment_two.py
@@ -1,15 +1,19 @@
-name = input("PLEASE STATE YOUR NAME.")
-print("nice name. I like it.")
-where=input("PLEASE STATE WHERE YOU ARE FROM.")
+name = input("PLEASE STATE YOUR NAME. ")
+print("nice name. I like it. ")
+where=input("PLEASE STATE WHERE YOU ARE FROM. ")
 print("I have been to", where,"many times. I hope to go again soon.")
-favnum = int(input("AS SIMPLY AS POSSIBLE, TELL ME YOUR FAVORITE NUMBER"))
+favnum = int(input("AS SIMPLY AS POSSIBLE, TELL ME YOUR FAVORITE NUMBER. "))
 
-favcar = input("WHAT IS YOUR DREAM CAR MODEL, BRAND, AND YEAR?")
+favcar = input("WHAT IS YOUR DREAM CAR MODEL, BRAND, AND YEAR? ")
 
-carabs = float(input("WHAT IS THE TOTAL PRICE OF THIS CAR?"))
+carabs = float(input("WHAT IS THE TOTAL PRICE OF THIS CAR? "))
 
-carmon = float(input("WHAT IS THE MONTHLY INTEREST OF THIS CAR?"))
+carmon = float(input("WHAT IS THE MONTHLY INTEREST OF THIS CAR? "))
 
 howmany = float(input("HOW MANY PAYMENTS DO YOU NEED TO MAKE?"))
 
 mpymt = (carmon * carabs) / (1 - (carmon + 1)**-howmany)
+
+print("THE TOTAL COST OF",favcar,"WILL BE",mpymt,)
+
+print("GOOD LUCK")
